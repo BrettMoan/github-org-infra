abdaa038829940c54e72539ff74ecac7d3ce6516
Brett Moan <6159590+BrettMoan@users.noreply.github.com>
2026-01-11 13:00:20 -0700

    remove imports

--- main.tf
+++ main.tf
@@ -36,32 +36,36 @@ module "repos" {
   auto_init   = each.value.auto_init
 }
 
-import {
-  to = module.repos["github-org-infra"].github_repository.this
-  id = "github-org-infra"
-}
+# The following import blocks are commented out, as they are no longere needed
+# these are needed if you want to import existing repositories into the Terraform state. 
+# Run imports like below if you have existing repositories that you want to manage with Terraform.
 
-import {
-  to = module.repos["iam-oidc-permissions"].github_repository.this
-  id = "iam-oidc-permissions"
-}
+# import {
+#   to = module.repos["github-org-infra"].github_repository.this
+#   id = "github-org-infra"
+# }
 
-import {
-  to = module.repos["frontend-app"].github_repository.this
-  id = "frontend-app"
-}
+# import {
+#   to = module.repos["iam-oidc-permissions"].github_repository.this
+#   id = "iam-oidc-permissions"
+# }
 
-import {
-  to = module.repos["network-config"].github_repository.this
-  id = "network-config"
-}
+# import {
+#   to = module.repos["frontend-app"].github_repository.this
+#   id = "frontend-app"
+# }
 
-import {
-  to = module.repos["backend-api"].github_repository.this
-  id = "backend-api"
-}
+# import {
+#   to = module.repos["network-config"].github_repository.this
+#   id = "network-config"
+# }
 
-import {
-  to = module.repos["reporting-analytics"].github_repository.this
-  id = "reporting-analytics"
-}
+# import {
+#   to = module.repos["backend-api"].github_repository.this
+#   id = "backend-api"
+# }
+
+# import {
+#   to = module.repos["reporting-analytics"].github_repository.this
+#   id = "reporting-analytics"
+# }
